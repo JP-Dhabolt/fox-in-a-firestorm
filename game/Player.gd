e7e82747843562830b75b74445dbf084791b1ce0
JP Dhabolt <john.p.dhabolt@gmail.com>
2022-11-25 11:25:35 -0500

    Add pouncing controls

--- game/Player.gd
+++ game/Player.gd
@@ -3,24 +3,55 @@ extends KinematicBody2D
 var GRAVITY: int = 15
 var MOVEMENT_SPEED: int = 150
 var JUMP_FORCE: int = 200
+var POUNCE_FORCE: int = 50
 
 var jumpAnimation: String = "Jump"
 var runAnimation: String = "Run"
 var idleAnimation: String = "Idle"
+var crouchAnimation: String = "Crouch"
+var hurtAnimation: String = "Hurt"
+var pounceAnimation: String = "Pounce"
+
+enum State {
+    NORMAL,
+    JUMPING,
+    POUNCING,
+    CROUCHING,
+    HURTING,
+}
 
 var velocity: Vector2 = Vector2.ZERO
+var currentState = State.NORMAL
 
 onready var sprite := $AnimatedSprite as AnimatedSprite
 
 
 func _physics_process(_delta):
 	velocity.y += GRAVITY
-	_handle_inputs()
+	_handle_shared_input()
+	_handle_state()
 	velocity = move_and_slide(velocity, Vector2.UP)
-	_animate()
 
 
-func _handle_inputs():
+func _handle_state():
+	match currentState:
+		State.NORMAL:
+			_handle_normal_state()
+		State.JUMPING:
+			_handle_jumping_state()
+		State.POUNCING:
+			_handle_pouncing_state()
+		State.CROUCHING:
+			_handle_crouching_state()
+		State.HURTING:
+			_handle_hurting_state()
+
+
+func _transition_to_normal():
+	currentState = State.NORMAL
+
+
+func _handle_shared_input():
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = MOVEMENT_SPEED
 	elif Input.is_action_pressed("ui_left"):
@@ -28,15 +59,44 @@ func _handle_inputs():
 	else:
 		velocity.x = 0
 
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = -JUMP_FORCE
 
-
-func _animate():
-	if is_on_floor():
-		if velocity.x != 0:
-			sprite.play(runAnimation)
-		else:
-			sprite.play(idleAnimation)
+func _handle_normal_state():
+	if velocity.x != 0:
+		sprite.play(runAnimation)
 	else:
-		sprite.play(jumpAnimation)
+		sprite.play(idleAnimation)
+
+	if Input.is_action_just_pressed("ui_up"):
+		_transition_to_jumping()
+
+
+func _transition_to_jumping():
+	currentState = State.JUMPING
+	velocity.y = -JUMP_FORCE
+	sprite.play(jumpAnimation)
+
+
+func _handle_jumping_state():
+	if is_on_floor():
+		_transition_to_normal()
+	if Input.is_action_just_pressed("ui_up"):
+		_transition_to_pouncing()
+
+
+func _transition_to_pouncing():
+	sprite.play(pounceAnimation)
+	velocity.y += POUNCE_FORCE
+	currentState = State.POUNCING
+
+
+func _handle_pouncing_state():
+	if is_on_floor():
+		_transition_to_normal()
+
+
+func _handle_crouching_state():
+	pass
+
+
+func _handle_hurting_state():
+	pass
