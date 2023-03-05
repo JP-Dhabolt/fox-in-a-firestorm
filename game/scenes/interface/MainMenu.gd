47be0ba9bbb097ae7d3227edff6b8b4fc64f2e73
JP Dhabolt <john.p.dhabolt@gmail.com>
2023-03-04 20:58:32 -0500

    Add start screen with main menu

---
+++ game/scenes/interface/MainMenu.gd
@@ -0,0 +1,12 @@
+extends ColorRect
+
+@onready var test_level_button: Button = find_child("TestLevelButton")
+@onready var quit_button: Button = find_child("QuitButton")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	test_level_button.pressed.connect(_load_test_level)
+	quit_button.pressed.connect(get_tree().quit)
+
+func _load_test_level():
+	get_tree().change_scene_to_file("res://scenes/levels/TestLevel.tscn")
