84065eb559826672b3d772040c3cfae000341a22
JP Dhabolt <john.p.dhabolt@gmail.com>
2023-08-31 20:41:37 -0400

    Add functional leaves #minor (#38)

--- game/scenes/characters/Player/Player.gd
+++ game/scenes/characters/Player/Player.gd
@@ -7,8 +7,12 @@ signal collided_with(node: Node2D)
 @export var movement_speed: int = 150
 @export var jump_force: int = 200
 @export var pounce_force: int = 50
+@export var food: int = 75
+@export var food_tick_rate: int = 2
 
 @onready var sprite := $AnimatedSprite2D as AnimatedSprite2D
+
+var elapsed_time: float = 0
 
 func _physics_process(_delta):
 	velocity.y += gravity
@@ -17,5 +21,15 @@ func _physics_process(_delta):
 	move_and_slide()
 	velocity = velocity
 
+func _process(delta: float):
+	elapsed_time += delta
+
+	if elapsed_time >= 1.0 / food_tick_rate:
+		elapsed_time -= 1.0 / food_tick_rate
+		food -= 1
+
 func on_collision(node: Node2D):
 	emit_signal("collided_with", node)
+
+func eat(amount: int):
+	food = min(100, food + amount)
---
+++ game/scenes/interface/HUD/Food.gd
@@ -0,0 +1,10 @@
+extends Label
+
+var player: Player
+func _ready():
+	await owner.ready
+	player = owner.player as Player
+	assert(player != null, "Player must be available to use this node")
+
+func _process(_delta):
+	text = "Food: " + str(player.food)
