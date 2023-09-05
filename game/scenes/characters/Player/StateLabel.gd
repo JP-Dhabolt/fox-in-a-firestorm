62bad9e0f356ffe6211dfd5c743ecd2c0ce34698
JP Dhabolt <john.p.dhabolt@gmail.com>
2023-09-04 21:24:24 -0400

    Refactor PlayerStateMachine

--- game/scenes/characters/Player/StateLabel.gd
+++ game/scenes/characters/Player/StateLabel.gd
@@ -1,8 +1,8 @@
 extends Label
 
 
-@export var state_machine: StateMachine
+@export var state_machine: PlayerStateMachine
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	text = state_machine.current_state.name
+	text = state_machine.current_state.state_name
