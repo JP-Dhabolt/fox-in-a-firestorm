554e4647fd10cf5180110004176814903ace61ee
JP Dhabolt <john.p.dhabolt@gmail.com>
2023-09-25 20:52:50 -0400

    Water collision is functional

--- game/scenes/systems/Water/WaterBody.gd
+++ game/scenes/systems/Water/WaterBody.gd
@@ -1,14 +1,16 @@
+@tool
 extends Polygon2D
 class_name WaterBody
 
-@export var depth: float = 1000
-@export var distance: float = 5
+@export var control_point_distance: float = 4
+
+@onready var collision: CollisionShape2D = $Area2D/CollisionShape2D
 
 func _ready():
 	polygon = PackedVector2Array()
 
-func draw_water(points: PackedVector2Array):
-	var water_body = _determine_curve(points, distance)
+func draw_water(points: PackedVector2Array, depth: float):
+	var water_body = _determine_curve(points, control_point_distance)
 	water_body.append(Vector2(points[-1].x, depth))
 	water_body.append(Vector2(points[0].x, depth))
 	polygon = water_body
@@ -27,7 +29,7 @@ func _determine_curve(input: PackedVector2Array, dist: float) -> PackedVector2Ar
 		curve.add_point(input[i], -dir * dist, dir * dist)
 
 	#calculate last point
-	var end_dir = input[-1].direction_to(input[-2])
+	var end_dir = input[-2].direction_to(input[-1])
 	curve.add_point(input[-1], - end_dir * dist, end_dir * dist)
 
 	return curve.get_baked_points()
