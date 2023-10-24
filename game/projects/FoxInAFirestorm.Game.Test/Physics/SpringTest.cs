025a1cf94031fde9d63b3d730a5c562ec147e028
JP Dhabolt <john.p.dhabolt@gmail.com>
2023-10-23 20:29:47 -0400

    Add Spring impl in Dotnet

---
+++ game/projects/FoxInAFirestorm.Game.Test/Physics/SpringTest.cs
@@ -0,0 +1,40 @@
+﻿using FoxInAFirestorm.Game.Physics;
+using Xunit;
+
+namespace FoxInAFirestorm.Game.Test.Physics;
+
+public class SpringTest
+{
+    [Fact]
+    public void DetermineNewY_Returns_As_Expected_After_Init()
+    {
+        double height = 100;
+        double constant = 0.010;
+        double dampingConstant = 0.01;
+        var uut = new Spring(constant, dampingConstant)
+        {
+            Height = height,
+        };
+        double targetHeight = 50;
+        var newY = uut.DetermineNewY(targetHeight);
+        double expectedNewY = (height - targetHeight) * -constant + height;
+        Assert.Equal(expectedNewY, newY, 0.00001);
+    }
+
+    [Fact]
+    public void DetermineNewY_Returns_As_Expected_With_Velocity()
+    {
+        double height = 100;
+        double constant = 0.015;
+        double dampingConstant = 0.01;
+        double startingVelocity = 10;
+        var uut = new Spring(constant, dampingConstant, velocity: startingVelocity)
+        {
+            Height = height,
+        };
+        double targetHeight = 50;
+        double expectedNewY = (height - targetHeight) * -constant + height - dampingConstant * startingVelocity + startingVelocity;
+        double newY = uut.DetermineNewY(targetHeight);
+        Assert.Equal(expectedNewY, newY, 0.00001);
+    }
+}
---
+++ game/projects/FoxInAFirestorm.Game/Physics/Spring.cs
@@ -0,0 +1,26 @@
+﻿namespace FoxInAFirestorm.Game.Physics;
+
+public class Spring
+{
+    public double Height { get; set; }
+    
+    private double _velocity;
+    private double _springConstant;
+    private double _dampingConstant;
+
+    public Spring(double springConstant, double dampingConstant, double velocity = 0)
+    {
+        _springConstant = springConstant;
+        _dampingConstant = dampingConstant;
+        _velocity = velocity;
+    }
+
+    public double DetermineNewY(double targetHeight)
+    {
+        double displacement = Height - targetHeight;
+        double loss = -_dampingConstant * _velocity;
+        double force = -_springConstant * displacement + loss;
+        _velocity += force;
+        return Height + _velocity;
+    }
+}
