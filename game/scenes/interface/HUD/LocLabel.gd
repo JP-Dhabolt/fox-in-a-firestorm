1b7bfcd9be1b1958f0b7ee5ed1409cc3de044d97
JP Dhabolt <john.p.dhabolt@gmail.com>
2024-10-09 21:07:57 -0400

    Convert TileMap to TileMapLayer

--- game/scenes/interface/HUD/LocLabel.gd
+++ game/scenes/interface/HUD/LocLabel.gd
@@ -1,6 +1,6 @@
 extends Label
 
-var tilemap: TileMap
+var ground_layer: TileMapLayer
 var player: Player
 
 func _ready():
@@ -8,9 +8,9 @@ func _ready():
 	player = owner.player as Player
 	assert(player != null, "Player must be available to use this node")
 	await owner.terrain_generator.ready
-	tilemap = owner.terrain_generator.tilemap as TileMap
-	assert(tilemap != null, "TerrainGenerator TileMape must be available to use this node")
+	ground_layer = owner.terrain_generator.ground_layer as TileMapLayer
+	assert(ground_layer != null, "TerrainGenerator GroundLayer must be available to use this node")
 
 func _process(_delta):
-	var tile_pos := tilemap.local_to_map(player.global_position)
+	var tile_pos := ground_layer.local_to_map(player.global_position)
 	text = "Loc: " + str(tile_pos.x) + ", " + str(tile_pos.y)
